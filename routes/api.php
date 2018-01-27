4af6a22b1a699f4c1392196eb8d0bbb5b8bff07e
Syukri Mullia Adil Perkasa <syukri.mullia@gmail.com>
2018-01-28 03:04:26 +0700

    added url for family registration request

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,7 @@ Route::group(['middleware' => 'auth:api'], function() {
     Route::apiResource('bank_accounts', 'BankAccountController');
     Route::apiResource('family', 'FamilyController');
     Route::apiResource('family_member', 'FamilyMemberController');
+    Route::apiResource('family_registration', 'FamilyRegistrationController');
 
     Route::get('user/{user_id}/get_bank_accounts', 'UserController@getBankAccounts');
     Route::post('connect_family', 'UserController@connectUserToFamily');
